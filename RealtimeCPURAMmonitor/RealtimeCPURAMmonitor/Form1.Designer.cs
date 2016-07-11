16637724cd0ad826d958676a3d99b76cd9361b4c
aleksandarbabic <alekski64@outlook.com>
2016-07-11 02:02:02 +0200

    CPU0-CPU3 monitor radi

--- RealtimeCPURAMmonitor/RealtimeCPURAMmonitor/Form1.Designer.cs
+++ RealtimeCPURAMmonitor/RealtimeCPURAMmonitor/Form1.Designer.cs
@@ -28,12 +28,265 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.progressBar6 = new System.Windows.Forms.ProgressBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.progressBar5 = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.performanceCounter1 = new System.Diagnostics.PerformanceCounter();
+            this.performanceCounter2 = new System.Diagnostics.PerformanceCounter();
+            this.performanceCounter3 = new System.Diagnostics.PerformanceCounter();
+            this.performanceCounter4 = new System.Diagnostics.PerformanceCounter();
+            this.performanceCounter5 = new System.Diagnostics.PerformanceCounter();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Pokreni praćenje";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(652, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 28);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Zaustavi praćenje";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.progressBar3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.progressBar4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.progressBar2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 62);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(791, 194);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CPU Monitor";
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(10, 118);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(775, 23);
+            this.progressBar3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CPU Core 3 - Usage";
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(10, 159);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(775, 23);
+            this.progressBar4.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "CPU Core 2 - Usage";
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(10, 76);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(775, 23);
+            this.progressBar2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CPU Core 1 - Usage";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(10, 36);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(775, 23);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CPU Core 0 - Usage";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.progressBar6);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.progressBar5);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(12, 262);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(791, 153);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "RAM Monitor";
+            // 
+            // progressBar6
+            // 
+            this.progressBar6.Location = new System.Drawing.Point(7, 96);
+            this.progressBar6.Name = "progressBar6";
+            this.progressBar6.Size = new System.Drawing.Size(775, 23);
+            this.progressBar6.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 80);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "%%%";
+            // 
+            // progressBar5
+            // 
+            this.progressBar5.Location = new System.Drawing.Point(7, 42);
+            this.progressBar5.Name = "progressBar5";
+            this.progressBar5.Size = new System.Drawing.Size(775, 23);
+            this.progressBar5.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "% RAM Usage in bytes";
+            // 
+            // performanceCounter1
+            // 
+            this.performanceCounter1.CategoryName = "Processor";
+            this.performanceCounter1.CounterName = "% Processor Time";
+            this.performanceCounter1.InstanceName = "0";
+            // 
+            // performanceCounter2
+            // 
+            this.performanceCounter2.CategoryName = "Processor";
+            this.performanceCounter2.CounterName = "% Processor Time";
+            this.performanceCounter2.InstanceName = "1";
+            // 
+            // performanceCounter3
+            // 
+            this.performanceCounter3.CategoryName = "Processor";
+            this.performanceCounter3.CounterName = "% Processor Time";
+            this.performanceCounter3.InstanceName = "2";
+            // 
+            // performanceCounter4
+            // 
+            this.performanceCounter4.CategoryName = "Processor";
+            this.performanceCounter4.CounterName = "% Processor Time";
+            this.performanceCounter4.InstanceName = "3";
+            // 
+            // performanceCounter5
+            // 
+            this.performanceCounter5.CategoryName = "Memory";
+            this.performanceCounter5.CounterName = "Committed Bytes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(815, 549);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Realtime CPU & RAM Monitor - ALPHA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter5)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ProgressBar progressBar5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ProgressBar progressBar6;
+        private System.Windows.Forms.Label label6;
+        private System.Diagnostics.PerformanceCounter performanceCounter1;
+        private System.Diagnostics.PerformanceCounter performanceCounter2;
+        private System.Diagnostics.PerformanceCounter performanceCounter3;
+        private System.Diagnostics.PerformanceCounter performanceCounter4;
+        private System.Diagnostics.PerformanceCounter performanceCounter5;
     }
 }
 
--- RealtimeCPURAMmonitor/RealtimeCPURAMmonitor/Form1.cs
+++ RealtimeCPURAMmonitor/RealtimeCPURAMmonitor/Form1.cs
@@ -7,14 +7,91 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace RealtimeCPURAMmonitor
 {
     public partial class Form1 : Form
     {
+        
+        Timer CPU0 = new Timer();
+        Timer CPU1 = new Timer();
+        Timer CPU2 = new Timer();
+        Timer CPU3 = new Timer();
+        Timer RAM = new Timer(); 
+        
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //once_loaded();
+            CPU0.Start();
+            CPU0.Interval = 300;
+            CPU0.Tick += new EventHandler(CPU0_tick);
+            CPU1.Start();
+            CPU1.Interval = 300;
+            CPU1.Tick += new EventHandler(CPU1_tick);
+            CPU2.Start();
+            CPU2.Interval = 300;
+            CPU2.Tick += new EventHandler(CPU2_tick);
+            CPU3.Start();
+            CPU3.Interval = 300;
+            CPU3.Tick += new EventHandler(CPU3_tick);
+            RAM.Start();
+        }
+        private void once_loaded () {
+            
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CPU0.Start();
+            CPU1.Start();
+            CPU2.Start();
+            CPU3.Start();
+            RAM.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CPU0.Stop();
+            CPU1.Stop();
+            CPU2.Stop();
+            CPU3.Stop();
+            RAM.Stop();
+        }
+        private void CPU0_tick(object sender, EventArgs e) {
+            label1.Text = string.Format("CPU Core 0 - Usage : {0}%",performanceCounter1.NextValue());
+            progressBar1.Value = (int)performanceCounter1.NextValue();
+        }
+        private void CPU1_tick(object sender, EventArgs e)
+        {
+            label2.Text = string.Format("CPU Core 1 - Usage : {0}%", performanceCounter2.NextValue());
+            progressBar2.Value = (int)performanceCounter2.NextValue();
+        }
+        private void CPU2_tick(object sender, EventArgs e)
+        {
+            label3.Text = string.Format("CPU Core 2 - Usage : {0}%", performanceCounter3.NextValue());
+            progressBar3.Value = (int)performanceCounter3.NextValue();
+        }
+        private void CPU3_tick(object sender, EventArgs e)
+        {
+            label4.Text = string.Format("CPU Core 3 - Usage : {0}%", performanceCounter4.NextValue());
+            progressBar4.Value = (int)performanceCounter4.NextValue();
         }
     }
 }
